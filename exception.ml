2b0e676df34335c19ced057aba6f8effbadf24a1
Darius Foo <darius.foo.tw@gmail.com>
2024-01-22 14:36:45 +0800

    applyN and exception

---
+++ exception.ml
@@ -0,0 +1,23 @@
+
+let failing (x:int ref) : int
+= x := !x + 1;
+  if !x = 3 then raise Not_found else !x
+(*@ r = failing x
+    requires !x > 0
+    raises Not_found -> !x > old !x
+    ensures !x > old !x
+*)
+
+let test1 () : bool =
+  let i = ref 2 in
+  let init = !i in
+  let z =
+    try
+      failing i
+    with Not_found ->
+      0
+  in
+  not (!i = init)
+(*@ r = test1 ()
+      ensures r = true
+*)
