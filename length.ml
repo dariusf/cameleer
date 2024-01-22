b82711510f37d9d3142f9b43653082b51b7c2d74
Darius Foo <darius.foo.tw@gmail.com>
2024-01-22 12:03:05 +0800

    Remaining

---
+++ length.ml
@@ -0,0 +1,9 @@
+
+(*@ function length (xs:'a list) : int =
+  match xs with
+  | [] -> 0
+  | _ :: xs1 -> 1 + length xs1 *)
+
+(*@ lemma length_positive_l: forall xs:'a list. length xs >=0 *)
+
+(*@ lemma length_emp: forall xs:'a list. xs=[] -> length xs = 0 *)
